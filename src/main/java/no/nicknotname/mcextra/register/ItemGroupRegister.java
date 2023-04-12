a4aa3e4f5448bb105c3038f25de95866a7733171
lwalton101 <waltonfamily01@outlook.com>
2023-04-12 19:05:36 +0100

    Made item group final

--- src/main/java/no/nicknotname/mcextra/register/ItemGroupRegister.java
+++ src/main/java/no/nicknotname/mcextra/register/ItemGroupRegister.java
@@ -7,7 +7,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ItemGroupRegister {
-    public static ItemGroup MCEXTRA_GROUP = FabricItemGroup.builder(new Identifier("mcextra","mcextra_group"))
+    public static final ItemGroup MCEXTRA_GROUP = FabricItemGroup.builder(new Identifier("mcextra","mcextra_group"))
             .displayName(Text.translatable("itemgroup.mcextra.mcextragroup"))
             .icon(() -> new ItemStack(ItemRegister.ancientEyeItem))
             .build();
